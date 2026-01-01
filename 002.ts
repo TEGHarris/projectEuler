87daff31ba05b3f89bf7d2399e67e36ef9d3bf91
Thomas Harris <harris.t.e.g@gmail.com>
2026-01-01 19:23:56 +0100

    problem 2 solved

---
+++ 002.ts
@@ -0,0 +1,18 @@
+const isEven = (n: number) => {
+  return n % 2 == 0;
+};
+const fibonacciNums: number[] = [1, 1];
+
+const generateFibonacciNum = (farr: number[]) => {
+  const len = farr.length;
+  const n = farr[len - 1] + farr[len - 2];
+  fibonacciNums.push(n);
+};
+while (fibonacciNums[fibonacciNums.length - 1] < 4000000) {
+  generateFibonacciNum(fibonacciNums);
+}
+console.log(fibonacciNums.length)
+console.log(fibonacciNums.reduce((acc:number,val:number) => {
+    if (isEven(val)) acc += val
+    return acc
+}, 0));
