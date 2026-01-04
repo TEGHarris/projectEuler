9d5028dff74104097735a079dd2fb3a96fc3d776
Thomas Harris <harris.t.e.g@gmail.com>
2026-01-04 16:19:50 +0000

    puzzle 4

---
+++ 004.ts
@@ -0,0 +1,13 @@
+let largestPalindrome = 0;
+
+const isPalindrome = (n: number) => {
+  let nString: string = n.toString();
+  const reversed = nString.split("").reverse().join("");
+  return nString == reversed;
+};
+for(let i = 100; i < 1000; i++){
+    for(let j = 100 ; j < 1000 ; j++){
+        if(isPalindrome(i*j) && i*j > largestPalindrome) largestPalindrome = i*j
+    }
+}
+console.log(largestPalindrome)
