55a3df888434a002dba189865c83adf5b3e264ff
Thomas Harris <harris.t.e.g@gmail.com>
2026-01-06 18:50:36 +0000

    implement solution for puzzle 6

---
+++ 006.ts
@@ -0,0 +1,15 @@
+const n = 100;
+
+const sumSquares = (n: number) => {
+  let total = 0;
+  for (let i = 1; i <= n; i++) total += i ** 2;
+  return total;
+};
+
+const squareSum = (n: number) => {
+  let total = 0;
+  for (let i = 1; i <= n; i++) total += i;
+  return total ** 2;
+};
+
+console.log(Math.abs(sumSquares(n) - squareSum(n)));
