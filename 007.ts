edb1f1603ab71cb3f87f24c582be035de1697383
Thomas Harris <harris.t.e.g@gmail.com>
2026-01-07 18:21:28 +0000

    Puzzle 7

---
+++ 007.ts
@@ -0,0 +1,18 @@
+const isPrime = (n: number) => {
+  for (let i = 2; i <= n ** 0.5; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+};
+
+
+let numPrimes = 0
+let counter = 1
+while (true){
+  counter++
+  if (isPrime(counter)) numPrimes++
+  if(numPrimes == 10001){
+    console.log(counter)
+    break
+  }
+}
