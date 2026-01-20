706215fbdf37c56343026ddaa5c6633a073302a7
Thomas Harris <164784721+TEGHarris@users.noreply.github.com>
2026-01-20 19:41:58 +0000

    Add incomplete comment to 022.ts
    Added a comment indicating that the code is incomplete.

--- 022.ts
+++ 022.ts
@@ -1,3 +1,4 @@
+// Incomplete
 import { readFileSync } from "fs";
 const alphabet = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 const names = readFileSync("0022_names.txt","utf-8").split(",").map((name : string) =>{return name.slice(1,name.length-1)}).sort()
